305d47aa490bd48eb87b7de1e7b52ec872037902
Mathupreetha2003 <727821tucs124@skct.edu.in>
2023-10-16 10:17:33 +0530

    reload

--- Day21/freelancer/src/Service/FreelancerService.js
+++
@@ -1,9 +0,0 @@
-import { freelancerApi } from "../api/axios";
-
-class FreelancerService{
-    getById(id){
-        return freelancerApi.get(`/getfreelancerbyid/${id}`);
-    }
-}
-
-export default new FreelancerService();
